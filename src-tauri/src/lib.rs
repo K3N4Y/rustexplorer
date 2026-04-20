d6cd8f2919a00867a53e04e8e1a7c16c05252593
Kenay Perez <Kenaygp1105@gmail.com>
2026-04-19 18:52:59 -0700

    Add get_files command and refactor search_with_ignore for improved file handling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,13 +3,13 @@ mod models;
 
 use crate::models::file_dto::FileDTO;
 use ignore::WalkBuilder;
-use std::sync::mpsc;
+use std::{fs, path::Path, sync::mpsc};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![search_with_ignore])
+        .invoke_handler(tauri::generate_handler![search_with_ignore, get_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -20,7 +20,7 @@ async fn search_with_ignore(pattern: String) -> Result<Vec<FileDTO>, String> {
         let (tx, rx) = mpsc::channel();
 
         let buscador = WalkBuilder::new("C:\\Users\\kenay\\OneDrive\\Desktop")
-            .threads(2) // evita saturar todos los nucleos
+            .threads(8) // evita saturar todos los nucleos
             .build_parallel();
 
         buscador.run(|| {
@@ -52,6 +52,27 @@ async fn search_with_ignore(pattern: String) -> Result<Vec<FileDTO>, String> {
     .map_err(|err| err.to_string())
 }
 
+#[tauri::command]
+fn get_files(path: String) -> Result<Vec<FileDTO>, String> {
+    read_one_level_files(Path::new(&path)).map_err(|err| err.to_string())
+}
 
-        
-    
+fn read_one_level_files(path: &Path) -> std::io::Result<Vec<FileDTO>> {
+    let mut files = Vec::new();
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+
+        let file_type = entry.file_type()?;
+
+        if file_type.is_file() || file_type.is_dir() {
+            files.push(FileDTO {
+                name: entry.file_name().to_string_lossy().to_string(),
+                path: entry.path(),
+            });
+        }
+    }
+
+    files.sort_by(|left, right| left.name.cmp(&right.name));
+    Ok(files)
+}
